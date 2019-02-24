39b85c08d9f666eccbfdc0ea79bf5a51c2baac38
krishnasagar14 <pagesagar@gmail.com>
2019-02-24 18:26:56 +0530

    Application secret key file enabled for prod

--- server/street_parking/settings/prod.py
+++ server/street_parking/settings/prod.py
@@ -1,10 +1,28 @@
 import os
+import string
+import random
 
 from .default import Default
 from configurations import values
 
+
+def get_app_key():
+    APP_KEY_FILE_PTH = 'sk.txt'
+    APP_KEY = None
+    random.seed(16)
+    if not os.path.isfile(APP_KEY_FILE_PTH):
+        string_choice = "{}{}{}{}".format(string.ascii_letters, string.digits, string.punctuation, string.hexdigits)
+        random_char_list = [random.SystemRandom().choice(string_choice) for i in range(50)]
+        APP_KEY = ''.join(random_char_list)
+        with open(APP_KEY_FILE_PTH, 'wb') as fobj:
+            fobj.write(APP_KEY.encode('utf-8'))
+    elif os.path.isfile(APP_KEY_FILE_PTH):
+        with open(APP_KEY_FILE_PTH, 'rb') as fobj:
+            APP_KEY = fobj.read().decode('utf-8')
+    return APP_KEY
+
 class Prod(Default):
-    SECRET_KEY = ''
+    SECRET_KEY = get_app_key()
 
     DEBUG = values.BooleanValue(False)
 
