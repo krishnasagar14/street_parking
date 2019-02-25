f00c854f68a51f21cc5676c2e6d3dec1bc129ec3
krishnasagar14 <pagesagar@gmail.com>
2019-02-25 08:22:49 +0530

    First cut of deployment - docker added

--- setup.sh
+++ setup.sh
@@ -2,12 +2,14 @@
 
 echo "Database setup on ubuntu system"
 
-sudo apt-get update
-sudo apt-get install mysql-server
+apt-get update
+apt-get install -y mysql-server
 
-sudo mysql <<EOF
+mysql <<EOF
 CREATE USER street_parking@localhost IDENTIFIED BY 'street_parking';
 GRANT ALL PRIVILEGES ON street_parking.* TO street_parking@localhost;
 FLUSH PRIVILEGES;
 CREATE DATABASE street_parking;
 EOF
+
+pip install -R requirements.txt
--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ echo "Street parking API service start"
 
 cd server;
 
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3 manage.py runserver
